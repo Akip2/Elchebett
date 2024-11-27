20575034ee74c334cc73580eb54442128d99a193
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2024-11-28 00:13:53 +0100

    Classes Party et PartyManager pour la gestion des joueurs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const PartyManager = require('./party-manager');
+const partyManager=new PartyManager();
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-//io = require('socket.io').listen(server, {
 
 var io = require('socket.io').listen(server, {
     //path: '/test',
@@ -20,15 +22,14 @@ app.get('/', function(req, res) {
 app.use(express.static(path.join(__dirname)));
 
 io.sockets.on('connection', function (socket) {
-
     console.log('Socket connected: ' + socket.conn.remoteAddress);
 
-    socket.on('test', function (val) {
-        console.log('Received ping: ' + val);
-    });
+    partyManager.addPlayer(socket);
 
-    socket.on('disconnect', function(socket) {
+    socket.on('disconnect', function() {
         console.log('Socket disconnected');
+
+        partyManager.removePlayer(socket);
     });
 });
 
